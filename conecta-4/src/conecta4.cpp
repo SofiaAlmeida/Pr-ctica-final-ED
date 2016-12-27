b09fb01181b20f0c67be68a0ef10a87453d183b8
SofíaAlmeida <sofiil97@gmail.com>
2016-12-27 13:26:51 +0000

    Cambio al comprobar argumentos, añadidos más comentarios

--- conecta-4/src/conecta4.cpp
+++ conecta-4/src/conecta4.cpp
@@ -1,4 +1,3 @@
-ce
 #include <iostream>
 #include <vector>
 #include <ctime>
@@ -52,40 +51,43 @@ void imprimeTablero(Tablero & t, Mando & m){
     cout << m.GetMando() << endl;
 }
 
-
-
 /******************************************************************************/
 /**
- * @brief Implementa el desarrollo de una partida de Conecta 4 sobre un tablero 5x7, pidiendo por teclado los movimientos de ambos jugadores según turno.
+ * @brief Implementa el desarrollo de una partida de Conecta 4 sobre un tablero filasxcolumnas, pidiendo por teclado los movimientos de ambos jugadores según turno.
  * @param filas Número de filas del tablero
  * @param columnas Número de columnas del tablero
  * @param metrica Métrica usada por el jugador automático
  * @param turno Turno del jugador que iniciará la partida
- * @return : Identificador (int) del jugador que gana la partida (1 o 2).
+ * @return: Identificador (int) del jugador que gana la partida (1 o 2).
  */
-int jugar_partida(int filas, int columnas, int metrica, int turno) {
-               //(filas, columnas, metrica, turno)
-  Tablero tablero(filas, columnas, turno);      //Tablero filasxcolumnas
-    Mando mando(tablero);       //Mando para controlar E/S de tablero
-    char c = 1;
-    int quienGana = tablero.quienGana();
-    //mientras no haya ganador y no se pulse tecla de terminación
-    while(c != Mando::KB_ESCAPE && quienGana == 0) {
-        system("clear");
-        mando.actualizarJuego(c, tablero);  // actualiza tablero según comando c 
-        imprimeTablero(tablero, mando);     // muestra tablero y mando en pantalla
-        quienGana = tablero.quienGana();    // hay ganador?
-        if(quienGana==0) //
-	  //if (turno == jugador automático) //para lo del turno podríamos decir que el jugador automático es siempre el 1 y el otro siempre el 2 y que eso sea lo que reciba como argumento al llamar al programa en <turno>, simplemente al crear el tablero tendría que empezar el jugador que diga turno
-	  //c = jugadorautomatico.columna()
-	  //else
-	  c = getch();       // Capturamos la tecla pulsada.    
-    }
+int jugar_partida(int filas = 5, int columnas = 7, int metrica = 1, int turno = 1) {
+  //(filas, columnas, metrica, turno)
+  Tablero tablero(filas, columnas, turno);      //Tablero filasxcolumnas 
+  Mando mando(tablero);       //Mando para controlar E/S de tablero
+  char c = 1;
+  int quienGana = tablero.quienGana();
+  //mientras no haya ganador y no se pulse tecla de terminación
+  while(c != Mando::KB_ESCAPE && quienGana == 0) {
+    system("clear");
+    mando.actualizarJuego(c, tablero);  // actualiza tablero según comando c 
+    imprimeTablero(tablero, mando);     // muestra tablero y mando en pantalla
+    quienGana = tablero.quienGana();    // ¿hay ganador?
+    //De alguna forma habría que inicializar el jugador automático según la métrica ¿pasándoselo como parámetro al pedirle la columna?
+    //se me ocurre tener un método columna que sea simplemente unos if-else comprobando el nº de métrica y llamando al método adecuado, que devolvería la posicion a insertar
+    if(quienGana == 0)
+      //if (turno == jugador automático) //para lo del turno podríamos decir que el jugador automático es siempre el 1 y el otro siempre el 2 y que eso sea lo que reciba como argumento al llamar al programa en <turno>
+      //c = jugadorautomatico.columna()
+      //else
+      c = getch();       // Capturamos la tecla pulsada.    
+  }
 
-    return tablero.quienGana();
+  return tablero.quienGana();
 }
 
 //metrica_ultima()
+//aleatoria
+
+//metrica_penultima()
 //verificar únicamente si en alguna fila gana o como segundo caso evita perder, si no insertar de forma aleatoria
 
 int main(int argc, char *argv[]){
@@ -93,19 +95,22 @@ int main(int argc, char *argv[]){
   int ganador;
   
   if(argc == 1)
-  //Comprobar argumentos, cambiar parámetros de funciones
-    ganador = jugar_partida(5,7,1,1);
-
+    ganador = jugar_partida();
+  else if(argc == 2)
+    ganador = jugar_partida(stoi(argv[1]));
+  else if(argc == 3)
+    ganador = jugar_partida(stoi(argv[1]), stoi(argv[2]));
+  else if(argc == 4)
+    ganador = jugar_partida(stoi(argv[1]), stoi(argv[2]), stoi(argv[3]));
   else if(argc == 5)
-    ganador = jugar_partida(fil, cols, metrica, turno);
-
+    ganador = jugar_partida(stoi(argv[1]), stoi(argv[2]), stoi(argv[3]), stoi(argv[4]));
   else {
     cout << "Número de argumentos incorrecto, por favor introduzca un número adecuado" << endl;
+    cout << "prompt %> conecta4 <filas_tablero> <cols_tablero> <metrica> <turno>" << endl;
     return 1;
   }
 
-  //prompt %> conecta4 <filas_tablero> <cols_tablero> <metrica> <turno>
-    cout << "Ha ganado el jugador " << ganador << endl;
+  cout << "Ha ganado el jugador " << ganador << endl;
 }  
   
   
